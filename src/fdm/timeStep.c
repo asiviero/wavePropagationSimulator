4c86685df7dc121bde2626df9ada69b0f13a0c70
Andre Siviero <altsiviero@gmail.com>
2012-04-14 03:42:25 -0300

    CSR code added, initializing, destroying and calculating k+1 matrix ok

--- src/fdm/timeStep.c
+++ src/fdm/timeStep.c
@@ -26,6 +26,8 @@ void loadUKP1(structMatrix sM) {
 	}
 }
 
+// This function loads the CSR version for U[k+1] matrix. It's a little tricky but I've tried
+// to use comprehensible variable names.
 void loadUKP1CSR(CSR_Matrix csrm) {
 
 	int sqrtdim = sqrt(csrm->dimension);
@@ -35,22 +37,36 @@ void loadUKP1CSR(CSR_Matrix csrm) {
 
 	int line_number,in_row_index,column_number,in_row_value;
 	float t;
+
 	in_row_index=1; column_number=0;
 	in_row_value=0;
+
+	// This loop fills the first N_HNODES from the matrix
+	// since they're all in a boundary condition, they're
+	// all 1s, one at each line, so it gets quite simple.
+	// It also has the side effect to alter the value of line_number
+	// This is the only loop with side effects
 	for(line_number=0; line_number<sqrtdim; line_number++) {
 		csrm->CSRVector[VALUES][line_number] = 1;
 		csrm->CSRVector[COLUMN][line_number] = line_number;
 		csrm->CSRVector[IN_ROW][in_row_index] = ++in_row_value;
 		in_row_index++; column_number++;
 	}
-	//j++;
 
+	// Once the "lower bound" is filled, we proceed to fill the other lines from the mesh
+	// Since we fill it horizontally, the first and last nodes are boundary nodes, which
+	// will be clear in the code below. Then we fill the inner nodes, whose lines on the
+	// matrix will have 5 non zero elements (bi-dimensional case).
 	for(int i=0; i<sqrtdim-2;i++) {
+
+		// First node, boundary condition
 		csrm->CSRVector[VALUES][line_number] = 1;
 		csrm->CSRVector[COLUMN][line_number] = column_number;
 		csrm->CSRVector[IN_ROW][in_row_index] = ++in_row_value;
+
 		in_row_index++; line_number++; column_number++;
 
+		// Inner nodes. In square meshes, there're are sqrtdim-2 inner nodes per line.
 		for(int j=0; j < sqrtdim -2; j++) {
 			in_row_value += 5;
 			csrm->CSRVector[VALUES][line_number] = 1/(2*pow(fDelta[Y_AXIS],2));
@@ -59,6 +75,9 @@ void loadUKP1CSR(CSR_Matrix csrm) {
 			csrm->CSRVector[VALUES][line_number+1] = 1/(2*pow(fDelta[X_AXIS],2));
 			csrm->CSRVector[COLUMN][line_number+1] = column_number-1;
 
+			// t is a parameter which identifies the node position and it's used by waveSpeed function
+			// todo alter the second parameter of waveSpeed to include time variation. It'll also be needed
+			// to change the function parameters
 			t = line_number*sqrtdim+line_number;
 			csrm->CSRVector[VALUES][line_number+2] = (-1/(pow(fDelta[X_AXIS],2)) -1/(pow(fDelta[Y_AXIS],2)) + 2/(waveSpeed(t,0)*pow(DELTA_TIME,2)));
 			csrm->CSRVector[COLUMN][line_number+2] = column_number;
@@ -73,12 +92,16 @@ void loadUKP1CSR(CSR_Matrix csrm) {
 			in_row_index++; column_number++;
 		}
 
+		// Last node in the line, it's also a boundary node
 		csrm->CSRVector[VALUES][line_number] = 1;
+
 		csrm->CSRVector[COLUMN][line_number] = column_number;
 		csrm->CSRVector[IN_ROW][in_row_index] = ++in_row_value;
 		in_row_index++; line_number++; column_number++;
 	}
 
+	// Once the inner lines are filled, we proceed to the last line, whose nodes are boundary nodes. This loop is similar to
+	// the first one, however the iterators have changed
 	for(int i=0; i<sqrtdim; i++) {
 			csrm->CSRVector[VALUES][line_number] = 1;
 			csrm->CSRVector[COLUMN][line_number] = line_number;
